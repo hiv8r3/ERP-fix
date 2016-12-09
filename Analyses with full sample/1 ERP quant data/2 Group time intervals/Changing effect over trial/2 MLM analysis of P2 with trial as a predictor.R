2c22a87c720ea3c6bea509b4fb73d78747fa62e6
hiv8r3 <hannah.volpert@mail.missouri.edu>
2016-12-09 17:38:06 -0600

    Adding analyses of simple slopes for change in P2 amp over course of experiment

--- Analyses with full sample/1 ERP quant data/2 Group time intervals/Changing effect over trial/2 MLM analysis of P2 with trial as a predictor.R
+++ Analyses with full sample/1 ERP quant data/2 Group time intervals/Changing effect over trial/2 MLM analysis of P2 with trial as a predictor.R
@@ -36,92 +36,72 @@ sink()
 
 
 
-# test simple slopes ------------------------------------------------------
-
-# # Example
-# data("Orthodont",package="MEMSS")
-# fm2 <- lmer(
-#   formula = distance ~ age*Sex + (age|Subject)
-#   , data = Orthodont
-# )
-# newdat2 <- expand.grid(
-#   age=c(8,10,12,14)
-#   , Sex=c("Female","Male")
-#   , distance = 0
-# )
-# 
-# mm2 <- model.matrix(terms(fm2),newdat2)
-# newdat2$distance <- predict(fm2,newdat2,re.form=NA)
-# #newdat$distance <- mm %*% fixef(fm1)
-# pvar2 <- diag(mm2 %*% tcrossprod(vcov(fm2),mm2))
-# cmult <- 2 ## could use 1.96
-# newdat <- data.frame(
-#   newdat,
-#   plo = newdat$distance-cmult*sqrt(pvar2),
-#   phi = newdat$distance+cmult*sqrt(pvar2)
-# )
-# #plot confidence
-# g0 <- ggplot(newdat, aes(x=age, y=distance, colour=Sex))+geom_point() +
-#   g0 + geom_errorbar(aes(ymin = plo, ymax = phi))+
-#   labs(title="CI based on fixed-effects uncertainty ONLY")
+# test simple slopes for eval ------------------------------------------------------
 
 
 fm1 <- lmer(MeanAmp ~ Race * Fix * rescaleTrial + (Race*Fix|Subject) + (1|Electrode:Subject), data = evaldat)
-newdat <- expand.grid(
-  Race = c("Black", "White"), 
-  Fix=c("eyes","forehead"),
-  rescaleTrial=1:512,
-  MeanAmp = 0
-)
 
-mm <- model.matrix(terms(fm1),newdat)
-newdat$MeanAmp <- predict(fm1,newdat,re.form=NA)
-#newdat$distance <- mm %*% fixef(fm1)
-pvar1 <- diag(mm %*% tcrossprod(vcov(fm1),mm))
-cmult <- 2 ## could use 1.96
-newdat <- data.frame(
-  newdat, 
-  plo = newdat$MeanAmp-cmult*sqrt(pvar1),
-  phi = newdat$MeanAmp+cmult*sqrt(pvar1)
-)
+parms <- fixef(fm1) # fixed parameters from model
+vcmat <- vcov(fm1) # variance/covariance matrix of fixed effects
 
-newdat$Condition <- paste(newdat$Race, newdat$Fix, sep=", ")
+## each row of this matrix defines entries of parms that we want to combine:
+Tmat <- matrix(NA,4,8)
+Tmat[1,] <- c(rep(0,3),1,0,0,0,0) # weights for estimates for race = 0, fix = 0 (Black-eyes)
+Tmat[2,] <- c(rep(0,3),1,0,0,1,0) # weights for estimates for race = 0, fix = 1 (Black-forehead)
+Tmat[3,] <- c(rep(0,3),1,0,1,0,0) # weights for estimates for race = 1, fix = 0 (White-eyes)
+Tmat[4,] <- c(rep(0,3),1,0,1,1,1) # weights for estimates for race = 1, fix = 1 (White-forehead)
 
-require(ggplot2)
-g0 <- ggplot(newdat[newdat$rescaleTrial<20,], aes(x=rescaleTrial, y=MeanAmp, colour=Condition))+geom_line() 
-g0 + geom_errorbar(aes(ymin = plo, ymax = phi))+
-  labs(title="CI based on fixed-effects uncertainty ONLY")
+parest <- Tmat %*% parms          # %*% is matrix multiplication. Uses the weights in Tmat to do the subtraction/addition of the estimates for you
+newvc <- Tmat %*% vcmat %*% t(Tmat) # shortcut to the usual calculation: Var(x + y) = Var(x) + Var(y) + 2*cov(x,y)
+ses <- sqrt(diag(newvc))            # to calculate SE
 
-write.table(newdat, paste(filepath,"confInt.txt", sep=""), row.names=F,sep="\t")
+## final results
+fin = cbind(parest, ses) %>% as.data.frame()
+names(fin) = c("est", "ses")
 
-head(coef(fm1))
+# to calculate 95% CI intervals, lower bound = m - 2*SE, upper bound = m + 2*SE
 
+fin$lbnd = fin$est - 2*fin$ses
+fin$ubnd = fin$est + 2*fin$ses
 
-# from Ed -----------------------------------------------------------------
+fin = format(fin, digits = 3)
 
-g0 <- ggplot(evaldat, aes(x=rescaleTrial, y=MeanAmp, colour=Condition)) + geom_line()
+fin$condition = c("Black-eyes", "Black-forehead", "White-eyes", "White-forehead")
+
+write.table(fin, paste(filepath,"Eval_confInt.txt", sep=""), row.names=F,sep="\t")
 
 
-
-# what I’m approximately looking for? --------------------------------------
-
-lmer(MeanAmp ~ Race * Fix * rescaleTrial + (Race*Fix|Subject) + (1|Electrode:Subject), data = evaldat) %>% 
-  summary()
+# test simple slopes for cat ------------------------------------------------------
 
 
-lmer(MeanAmp ~ rescaleTrial + (1|Subject) + (1|Electrode:Subject), # b = .171
-     data = evaldat[evaldat$Condition == "Black_eyes",]) %>% 
-  summary()
+fm2 <- lmer(MeanAmp ~ Race * Fix * rescaleTrial + (Race*Fix|Subject) + (1|Electrode:Subject), data = catdat)
 
-lmer(MeanAmp ~ rescaleTrial + (1|Subject) + (1|Electrode:Subject), # b = .101
-     data = evaldat[evaldat$Condition == "Black_forehead",]) %>% 
-  summary()
+parms <- fixef(fm2) # fixed parameters from model
+vcmat <- vcov(fm2) # variance/covariance matrix of fixed effects
 
-lmer(MeanAmp ~ rescaleTrial + (1|Subject) + (1|Electrode:Subject), # b = .104
-     data = evaldat[evaldat$Condition == "White_eyes",]) %>% 
-  summary()
+## each row of this matrix defines entries of parms that we want to combine:
+Tmat <- matrix(NA,4,8)
+Tmat[1,] <- c(rep(0,3),1,0,0,0,0) # weights for estimates for race = 0, fix = 0 (Black-eyes)
+Tmat[2,] <- c(rep(0,3),1,0,0,1,0) # weights for estimates for race = 0, fix = 1 (Black-forehead)
+Tmat[3,] <- c(rep(0,3),1,0,1,0,0) # weights for estimates for race = 1, fix = 0 (White-eyes)
+Tmat[4,] <- c(rep(0,3),1,0,1,1,1) # weights for estimates for race = 1, fix = 1 (White-forehead)
 
-lmer(MeanAmp ~ rescaleTrial + (1|Subject) + (1|Electrode:Subject), # b = .103
-     data = evaldat[evaldat$Condition == "White_forehead",]) %>% 
-  summary()
+parest <- Tmat %*% parms          # see above for notation
+newvc <- Tmat %*% vcmat %*% t(Tmat)
+ses <- sqrt(diag(newvc)) 
+
+## final results
+fin = cbind(parest, ses) %>% as.data.frame()
+names(fin) = c("est", "ses")
+
+# to calculate 95% CI intervals, lower bound = m - 2*SE, upper bound = m + 2*SE
+
+fin$lbnd = fin$est - 2*fin$ses
+fin$ubnd = fin$est + 2*fin$ses
+
+fin = format(fin, digits = 3)
+
+fin$condition = c("Black-eyes", "Black-forehead", "White-eyes", "White-forehead")
+
+write.table(fin, paste(filepath,"Cat_confInt.txt", sep=""), row.names=F,sep="\t")
 
